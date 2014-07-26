b23b7b23954c6126738fb2aa961718df8497d376
Coinworksdotinfo <adamandshandra@server.local>
2014-07-26 15:22:59 -0600

    MUE 1.0

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 3,
+        PUBKEY_ADDRESS = 15,
         SCRIPT_ADDRESS = 9,
         PUBKEY_ADDRESS_TEST = 119,
         SCRIPT_ADDRESS_TEST = 199,
